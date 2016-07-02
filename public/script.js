db53b9caa42d994f711a9ec7d1729f07d7562e27
Elianne Schutze <eliannemsr@gmail.com>
2016-07-02 15:57:38 +0100

    add single post pages, add blog title

--- public/script.js
+++ public/script.js
@@ -7,18 +7,26 @@ $(document).ready(function() {
             for (var blogPost in data) {
                 var postDiv         = document.createElement('div');
                 var postText        = document.createElement('p');
+                var postLink = document.createElement('a');
+                var postTitle        = document.createElement('h3');
                 var thumbnail       = document.createElement('img');
                 var postContainer   = document.getElementsByClassName('post-container')[0];
 
                 thumbnail.src = "./img/logo2.png";
                 thumbnail.className = "thumbnail";
-                postText.innerHTML = data[blogPost];
+                postLink.setAttribute(
+                    "href", '/posts/'+blogPost
+                );
+                postTitle.innerHTML = data[blogPost].blogTitle;
+                postText.innerHTML = data[blogPost].blogpost ;
                 postDiv.className = "post";
 
                 postDiv.appendChild(thumbnail);
+                postDiv.appendChild(postLink);
+                postLink.appendChild(postTitle);
                 postDiv.appendChild(postText);
                 postContainer.appendChild(postDiv);
-                
+
             }
         },
         error: function(error){
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var mustacheExpress = require('mustache-express');
+
+
+
 
 // To initialise our server, we just need to call the express() function. This will create an Express application for us to work with.
 var app = express()
@@ -12,12 +17,48 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
 
 app.post("/create-post",function(req,res){
-    console.log("create-post");
-    console.log(req.body);
+    res.redirect('/');
+    var myFile=__dirname+'/data/posts.json';
+    fs.readFile(myFile,function(error,file){
+        var parsedFile = JSON.parse(file);
+
+        parsedFile[Date.now()] = req.body;
+        // console.log(parsedFile);
+        var JSONText = JSON.stringify(parsedFile);
+        fs.writeFile(myFile,JSONText,function(error){
+            // console.log("imhere");
+
+
+        })
+
+    });
+
 });
 
+app.get('/get-posts', function(req,res){
+    res.sendFile(__dirname+'/data/posts.json');
+});
+
+app.get('/posts/:postId', function(req,res){
+    // var blogpost;
+    fs.readFile(__dirname+'/data/posts.json',function(error,file){
+        var fileObj = JSON.parse(file);
+        var key = req.params.postId
+        var blogpost = fileObj[key.toString()];
+        var title = JSON.stringify(blogpost.blogTitle).split("\"").join("");
+
+        res.render('post',{ title: title, body: blogpost.blogpost});
+    });
+
+
+});
 
 app.listen(3000, function(){
     console.log("Server is listening on Port 3000! Ready to accept requests.." );
